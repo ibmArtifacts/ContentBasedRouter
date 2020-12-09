98f8e7898f13045614b214ccb69d4ccfb7d6fed6
liamlynk <66093865+liamlynk@users.noreply.github.com>
2020-12-08 21:56:37 -0800

    Add files via upload

---
+++ router.js
@@ -0,0 +1,33 @@
+var hm = require('header-metadata');
+var service = require('service-metadata');
+
+//Read input
+session.input.readAsBuffers(function (error, input) {
+  if (error) {
+    // handle error
+    session.output.write (error.errorMessage);
+  }
+  else {
+	
+	if (input == '') {
+		session.output.write("Please enter something");
+		service.mpgw.skipBackside = true;
+	}	
+	
+	//If input contains "name", route traffic to /profile endpoint.
+	if (input.indexOf('name') > -1) {
+		service.setVar('var://service/routing-url', 'http://localhost:2000/profile');
+	}
+	
+	//If input contains "accounts", route traffic to /accounts endpoint.
+	if (input.indexOf('accounts') > -1) {
+		service.setVar('var://service/routing-url', 'http://localhost:2000/accounts');
+	}
+	
+	//If input contains "beneficiaries", route traffic to /beneficiaries endpoint.
+	if (input.indexOf('beneficiaries') > -1) {
+		service.setVar('var://service/routing-url', 'http://localhost:2000/beneficiaries');
+	}
+	
+  }
+});
